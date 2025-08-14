f6033cd00564583f9a667bd43d718aeda3d98a05
Ali Hamood <ali.hamood@gov.bc.ca>
2025-08-14 15:59:33 -0700

    Adds 404 page, adds job page. Added json-server as development server.

--- fin-manag-client/src/router/index.ts
+++ fin-manag-client/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Jobs from '../views/Jobs.vue';
+import Job from '../views/Job.vue';
+import NotFound from '../views/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +21,16 @@ const router = createRouter({
             path: '/jobs/add',
             name: 'jobs-add',
             component: Home
+        },
+        {
+            path: '/job/:id',
+            name: 'job',
+            component: Job
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: 'no-found',
+            component: NotFound
         }
     ]
 });
