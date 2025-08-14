408717bc3aeb3dd80ef56b86ce1b5feab702cc04
Ali Hamood <ali.hamood@gov.bc.ca>
2025-08-14 15:07:46 -0700

    Adds env.d.ts file to craete vue type. Adds router index.ts and updats app.vue.

--- fin-manag-client/src/App.vue
+++ fin-manag-client/src/App.vue
@@ -1,13 +1,7 @@
 <script setup lang='ts'>
   import NavBar from './components/NavBar.vue';
-  import Hero from './components/Hero.vue';
-  import HomeCards from './components/HomeCards.vue';
-  import JobListings from './components/JobListings.vue'
 </script>
 
 <template>
     <NavBar />
-    <Hero title="This is a Test Title." subtitle="This is a test sub title."/>
-    <HomeCards />
-    <JobListings :limit="3" :showButton="true"/>
 </template>
